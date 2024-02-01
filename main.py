764dc313a73dc5e538a7d9e6994481c22e628e26
fenna <fennazwaans@hotmail.com>
2024-02-01 14:13:55 +0100

    third try

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import numpy as np
 
 #testing fenna
+import pandas as pd
+data = pd.io.stata.read_stata("C:Users\fenna\Downloads\cs13f_2.0p_EN\cs13f_2.0p_EN.dta")
+data.to_csv('my_stata_file.csv')
+
